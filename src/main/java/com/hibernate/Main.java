1fcdb0eb772d70c574f7bded8945bf829a934a1c
Adam <adaskoo112@gmail.com>
2018-04-10 16:30:49 +0400

    new methods

--- src/main/java/com/hibernate/Main.java
+++ src/main/java/com/hibernate/Main.java
@@ -3,6 +3,8 @@ package com.hibernate;
 import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Iterator;
+import java.util.List;
 
 
 public class Main {
@@ -20,6 +22,8 @@ public class Main {
         Main mainCall = new Main();
 
         Integer intval = mainCall.addUser("Adam", "Alaba");
+        mainCall.listUsers();
+        mainCall.updateUser(2);
 
 
     }
@@ -46,7 +50,44 @@ public class Main {
     }
 
     //update user
-    // get users from db
+    public void updateUser(Integer userID){
+        Session session = factory.openSession();
+        Transaction tx = null;
 
+        try {
+            tx = session.beginTransaction();
+            User user = (User) session.get(User.class, userID);
+            session.update(user);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    // get users from db
+    public void listUsers( ){
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            List users = session.createQuery("FROM User").list();
+            for (Iterator iterator = users.iterator(); iterator.hasNext();){
+                User user = (User) iterator.next();
+                System.out.print("First Name: " + user.getFirstName());
+                System.out.print("  Last Name: " + user.getLastName());
+//                System.out.println("   ID: " + user.getId());
+            }
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
 
 }
